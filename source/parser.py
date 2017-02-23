9f9eaf07498de1a3d68a0f429190e3208a6d79c5
Shan Senanayake <senanayake92@gmail.com>
2017-02-23 19:19:44 +0100

    added class sturct

--- source/parser.py
+++ source/parser.py
@@ -1,5 +1,23 @@
 import sys
 
+class Ep:
+
+    def __init__(self, index, latency):
+        self.index = index
+        self.latency = latency
+        self.caches = list()
+
+    def add_cache(self, cache, latency):
+        self.caches.append((cache, latency))
+        cache.eps.append((self, latency))
+
+class Cache:
+
+    def __init__(self, index):
+        self.index = index
+        self.eps = list()
+
+
 
 def main(args):
     with open(args[0], 'r') as f:
